875c4bee18e68f65b8833cc7afec46c6b0831cee
Simon Light (シモネ) <SimonLight001@users.noreply.github.com>
2017-01-09 21:59:40 +0000

    transfer again?

--- Front Test/Main.js
+++ Front Test/Main.js
@@ -22,12 +22,15 @@ $(document).ready(function() {
     UpdateSize();
     RedrawCards();
     RepositionCards();
+	checkFileAPI();
+	AssignData();
 });
 
 $(window).resize(function() {
     UpdateSize();
     RedrawCards();
     RepositionCards();
+	AssignData();
 });
 
 function UpdateSize()
@@ -159,9 +162,84 @@ function RepositionCards()
     }
 }
 
+function AssignData()
+{
+	
+	var parser, xmlDoc;
+
+	text = "<bookstore><book>" +
+	"<title>Everyday Italian</title>" +
+	"<author>Giada De Laurentiis</author>" +
+	"<year>2005</year>" +
+	"</book></bookstore>";
+	
+	console.log(text);
+
+	parser = new DOMParser();
+	xmlDoc = parser.parseFromString(text,"text/xml");
+
+	document.getElementById("content1").innerHTML =
+	xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
+}
 function AddCard()
 {
 
 }
+
+    var reader; //GLOBAL File Reader object for demo purpose only
+
+    /**
+     * Check for the various File API support.
+     */
+    function checkFileAPI() {
+        if (window.File && window.FileReader && window.FileList && window.Blob) {
+            reader = new FileReader();
+            return true; 
+        } else {
+            alert('The File APIs are not fully supported by your browser. Fallback required.');
+            return false;
+        }
+    }
+
+    /**
+     * read text input
+     */
+    function readText(filePath) {
+        var output = ""; //placeholder for text output
+        if(filePath.files && filePath.files[0]) {           
+            reader.onload = function (e) {
+                output = e.target.result;
+                displayContents(output);
+            };//end onload()
+            reader.readAsText(filePath.files[0]);
+        }//end if html5 filelist support
+        else if(ActiveXObject && filePath) { //fallback to IE 6-8 support via ActiveX
+            try {
+                reader = new ActiveXObject("Scripting.FileSystemObject");
+                var file = reader.OpenTextFile(filePath, 1); //ActiveX File Object
+                output = file.ReadAll(); //text contents of file
+                file.Close(); //close file "input stream"
+                displayContents(output);
+            } catch (e) {
+                if (e.number == -2146827859) {
+                    alert('Unable to access local files due to browser security settings. ' + 
+                     'To overcome this, go to Tools->Internet Options->Security->Custom Level. ' + 
+                     'Find the setting for "Initialize and script ActiveX controls not marked as safe" and change it to "Enable" or "Prompt"'); 
+                }
+            }       
+        }
+        else { //this is where you could fallback to Java Applet, Flash or similar
+            return false;
+        }       
+        return true;
+    }   
+
+    /**
+     * display content using a basic HTML replacement
+     */
+    function displayContents(txt) {
+        var el = document.getElementById('main'); 
+        el.innerHTML = txt; //display output in DOM
+    }   
 
 //SLIDESHOW
